a0d106c72ddd48ae3fed7ddebf9f825e363324c6
bevennyamande <bevennyamande@gmail.com>
2019-04-06 10:21:52 +0200

    added methods

---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+
+class GetSwift(object):
+
+    def __init__(self):
+        pass
+
+    def book_delivery(self):
+        pass
+
+    def get_quote(self):
+        pass
+
+    def cancel_delivery(self):
+        pass
+
+    def track_order(self):
+        pass
+
+    def view_delivery_history(self):
+        pass
+
+    def get_driver_details(self):
+        pass
+
+    def list_drivers(self):
+        pass
+
+    def driver_proximity(self):
+        pass
+
+
+
